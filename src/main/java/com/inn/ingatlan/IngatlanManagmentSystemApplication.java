1c2c88c25cb03e58e3a7bfad245668f279279622
Gyuris László Márton <gylm99@gmail.com>
2023-03-15 18:49:24 +0100

    Első

---
+++ src/main/java/com/inn/ingatlan/IngatlanManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.ingatlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngatlanManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngatlanManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/ingatlan/IngatlanManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.ingatlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngatlanManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
